b5a35e8b82efde03970d02d30932cdc69e65bf70
Konstantin Tarletski <konsyantin.tarletski@gmail.com>
2026-01-05 22:21:17 +0200

    Adding some vars

--- alb_and_ssl/outputs.tf
+++ alb_and_ssl/outputs.tf
@@ -2,8 +2,9 @@ output "alb_dns_name" {
   value = aws_lb.alb.dns_name
 }
 
-output "target_groups" {
-  value = { for k, v in aws_lb_target_group.port_tg : k => v.arn }
+output "ports_with_target_groups" {
+  value       = { for k, v in aws_lb_target_group.port_tg : k => v.arn }
+  description = "Example: ['80080' = 'arn:tg-123']"
 }
 
 output "default_target_group_arn" {
@@ -12,6 +13,7 @@ output "default_target_group_arn" {
 
 output "ports_and_tg_arns_map" {
   value = { for k, v in aws_lb_target_group.port_tg : k => { tg_arn = v.arn } }
+  description = "Example: ['80080' = {tg_arn = 'arn:tg-123'}]"
 }
 
 output "alb_sg_id" {
